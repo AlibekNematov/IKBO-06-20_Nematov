cd271af883532faa542b4b5a22a00d261250fd5f
AlibekNematov <85082765+AlibekNematov@users.noreply.github.com>
2021-09-15 00:19:38 +0300

    Add files via upload
    Практическая работа номер 1, часть 1

--- company/Main.java
+++ company/Main.java
@@ -1,62 +1,27 @@
 package com.company;
-import java.util.Scanner;
 
-public class Main extends Person{
-    public static class Human {
-        private static String name;
-        private static  int height;
-        private static  int age;
+public class Main extends Book {
 
-        public Human(int h, String n) {
-            int height = h;
-            String name = n;
-        }
-
-        public static void setHuman() {
-            Scanner in = new Scanner(System.in);
-            System.out.println("Введите имя: ");
-            name = in.nextLine();
-            System.out.println("Введите рост: ");
-            height = in.nextInt();
-            System.out.println("Введите возраст: ");
-            age = in.nextInt();
-            if (age <= 0) {
-                System.out.println("Неправильный возраст");
-            }
-            else
-            {
-                System.out.println("Ваше имя: " + name);
-                System.out.println("Ваш возраст: " + age);
-                System.out.println("Ваш рост: " + height);
-            }
-        }
-    }
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-            System.out.println("Введите ваше имя: ");
-        name = in.nextLine();
-        setName(name);
-        if (setName(name)==false){
-            System.out.print("Имя не введено");
-            return;
-        }
-            System.out.println("Введите ваш рост: ");
-        height = in.nextInt();
-        setHeight(height);
-        if(setHeight(height)==false) {
-            System.out.print("Рост не введен, либо указан не в пределе от 45 до 250см");
-            return;
-        }
-            System.out.println("Введите ваш возраст: ");
-        age = in.nextInt();
-        setAge(age);
-        if(setAge(age)==false){
-            System.out.print("возраст не введен, либо указан не в пределе от 0 до 160");
-            return;
-        }
+        Book b1 = new Book();
+        Book b2 = new Book("Война и Мир");
+        Book b3 = new Book("Бедная Лиза", "Карамзин Николай Михайлович");
+        Book b4 = new Book("Убийство в восточном экспрессе", "Агата Кристи", "detective");
 
-            System.out.println("Ваше имя: " + getName());
-            System.out.println("Ваш рост: " + getHeight());
-            System.out.println("Ваш возраст: " + getAge());
+        b1.setName("Мастер и Маргарита");
+        b1.setAuthor("Михаил Булгаков");
+        b1.setGenre("roman");
+        b2.setAuthor("Лев Толстой");
+        b2.setGenre("russian classic");
+        b3.setGenre("novella");
+
+        System.out.println(b1);
+        b1.choice();
+        System.out.println(b2);
+        b2.choice();
+        System.out.println(b3);
+        b3.choice();
+        System.out.println(b4);
+        b4.choice();
     }
 }
